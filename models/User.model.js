2417c3f2ec33692753393a706e30899d4d2c0531
Karl97490 <karl.pery@icloud.com>
2026-06-25 13:19:43 +0400

    users routes: modify user informations excepted password and email

--- models/User.model.js
+++ models/User.model.js
@@ -5,10 +5,12 @@ const userSchema = new Schema(
     firstName: {
       type: String,
       required: true,
+      trim: true,
     },
     lastName: {
       type: String,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -22,4 +22,48 @@ router.get("/:userId", verifyToken, async (req, res, next) => {
   }
 });
 
+// PATCH /api/users/:userId
+router.patch("/:userId", verifyToken, async (req, res, next) => {
+  // Check if the id in the payload match with the id in the request params
+  if (req.payload._id !== req.params.userId) {
+    res.status(401).json({ message: "Unauthorized access." });
+    return;
+  }
+
+  const { firstName, lastName } = req.body;
+
+  if (!firstName || !lastName) {
+    res.status(400).json({ message: "First name and last name are required." });
+    return;
+  }
+
+  const nameRegex = /^[\p{L}]+(?:[ '-][\p{L}]+)*$/u;
+  if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+    res
+      .status(400)
+      .json({ message: "First name and last name are incorrect." });
+    return;
+  }
+
+  try {
+    const updatedUser = {
+      firstName,
+      lastName,
+    };
+    const response = await User.findByIdAndUpdate(
+      req.params.userId,
+      updatedUser,
+      { returnDocument: true, runValidators: true },
+    );
+    if (!response) {
+      res.status(400).json({ message: "User not found." });
+      return;
+    }
+
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
