5d73bdd6e33f33ff8ddb70c01e96c97ac07d3dd5
Karl97490 <karl.pery@icloud.com>
2026-06-25 20:15:08 +0400

    services routes: put route

--- routes/services.routes.js
+++ routes/services.routes.js
@@ -30,4 +30,45 @@ router.post("/", verifyToken, async (req, res, next) => {
   }
 });
 
+// PUT /api/services/:serviceId
+router.put("/:serviceId", verifyToken, async (req, res, next) => {
+  // console.log(req.body);
+  // console.log(req.payload._id);
+  // console.log(req.params.serviceId);
+  // res.send("PUT services route, all good here");
+  const { title, type, description, unitPrice } = req.body;
+
+  if (!title) {
+    res.status(400).json({ message: "Title is required." });
+    return;
+  }
+
+  try {
+    const updatedService = {
+      title,
+      type,
+      description,
+      unitPrice,
+    };
+    if (Object.values(updatedService).includes(undefined)) {
+      res.status(400).json({ message: "Incorrect request." });
+      return;
+    }
+
+    const response = await Service.findOneAndUpdate(
+      { _id: req.params.serviceId, ownerId: req.payload._id },
+      updatedService,
+      { returnDocument: true, runValidators: true },
+    );
+    if (!response) {
+      res.status(400).json({ message: "Service not found." });
+      return;
+    }
+
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
